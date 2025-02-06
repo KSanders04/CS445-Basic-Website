775bd3ea951c339b80b9480ff14e5c630a04ec1e
KSanders04 <kasanders2004@gmail.com>
2025-02-06 12:25:58 -0500

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const eggCount = ref(0)
+const tortillaCount = ref(0)
+const chocolateCount = ref(0)
+
+</script>
+
+<template>
+  <div>
+    Eggs
+    <button type="button" @click="eggCount--">-</button>
+    <button type="button" @click="eggCount++">+</button>
+    {{ eggCount }}
+    <p>
+    Tortilla
+    <button type="button" @click="tortillaCount--">-</button>
+    <button type="button" @click="tortillaCount++">+</button>
+    {{ tortillaCount }}
+    </p>
+    chocolate
+    <button type="button" @click="chocolateCount--">-</button>
+    <button type="button" @click="chocolateCount++">+</button>
+    {{ chocolateCount }}
+  </div>
+</template>
